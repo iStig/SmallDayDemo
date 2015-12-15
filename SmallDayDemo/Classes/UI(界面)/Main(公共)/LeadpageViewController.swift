25daec0b6cc0c2d1cfd3fef0fe167bc2934e7ea1
iStig <istig@iStigdeMacBook-Pro.local>
2015-12-15 15:49:39 +0800

    修改部分内容

---
+++ SmallDayDemo/Classes/UI(界面)/Main(公共)/LeadpageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LeadpageViewController.swift
+//  SmallDayDemo
+//
+//  Created by iStig on 15/12/12.
+//  Copyright © 2015年 iStig. All rights reserved.
+//
+
+import UIKit
+
+public let SD_ShowMainTabbarController_Notification = "SD_ShowMainTabbarController_Notification"
+
+class LeadpageViewController: UIViewController {
+  
+  private let backgroundImage = UIImageView(frame: MainBounds)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SmallDayDemo/Classes/UI(界面)/Other(其他)/theme.swift
@@ -0,0 +1,14 @@
+//
+//  theme.swift
+//  SmallDayDemo
+//
+//  Created by iStig on 15/12/12.
+//  Copyright © 2015年 iStig. All rights reserved.
+//
+
+import UIKit
+
+public let MainBounds: CGRect = UIScreen.mainScreen().bounds
+
+struct theme {
+}
