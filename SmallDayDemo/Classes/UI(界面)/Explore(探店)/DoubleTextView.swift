1063f310512843bbb1905240127f2f2ddc531846
iStig <istig@iStigdeMacBook-Pro.local>
2016-03-09 10:23:38 +0800

    add new files

---
+++ SmallDayDemo/Classes/UI(界面)/Explore(探店)/DoubleTextView.swift
@@ -0,0 +1,21 @@
+//
+//  DoubleTextView.swift
+//  SmallDayDemo
+//
+//  Created by iStig on 16/3/8.
+//  Copyright © 2016年 iStig. All rights reserved.
+//
+
+import UIKit
+
+class DoubleTextView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
